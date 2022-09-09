27b1d5fcda1c2eede9295495a2122789c5a24779
lucas <lucas@lucasPC>
2022-09-09 15:10:02 -0300

    comit2

---
+++ somarDoisInt/src/somar2numeros.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class somar2numeros {
+	
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubcx
+		int num1, num2, soma;
+		
+		System.out.println("Digite um numero:");
+		num1 = sc.nextInt();
+		
+		System.out.println("Digite outro numero:");
+		num2 = sc.nextInt();
+		
+		
+		soma = num1 + num2;
+		
+		System.out.println("Soma: "+soma);
+		
+	}
+
+}
